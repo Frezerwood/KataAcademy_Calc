7d3bb3b515fe390c59f1096b40c04d2a929a666c
Aleksandr <viperasl@mail.ru>
2022-06-13 01:29:16 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String inputExpression = scanner.next();
+        String[] elements  = inputExpression.split(" ");
+
+    }
+
+
+    public static String calc (String input) {
+        return null;
+    }
+}
---
+++ src/Numeric.java
@@ -0,0 +1,20 @@
+public enum Numeric {
+
+    I(1),
+    II(2),
+    III(3),
+    IV(4),
+    V(5),
+    VI(6),
+    VII(7),
+    VIII(8),
+    IX(9),
+    X(10);
+
+    private int arabic;
+
+    Numeric(int arabic) {
+        this.arabic = arabic;
+    }
+
+}
