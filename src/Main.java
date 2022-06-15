870fee538ad3aae68f9b196a66be2143d5c75ed3
Aleksandr <viperasl@mail.ru>
2022-06-16 02:09:12 +0400

    Double N1

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,79 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Введите число в формате a + b");
         Scanner scanner = new Scanner(System.in);
         String inputExpression = scanner.nextLine();
 
-        calc(inputExpression);
-
+        try {
+            System.out.println(calc(inputExpression));
+        } catch (Exception e) {
+            System.out.println("Программа завершена");
+        }
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws Exception {
 
         String[] elements = input.split(" ");
         int a = 0;
         int b = 0;
         char c = ' ';
+        boolean controlCheckbox = false;
+        Numeric[] romanNumbers = Numeric.values();       // Массив с римскими цифрами
 
-        try {
-            a = Integer.parseInt(elements[0]);
-            b = Integer.parseInt(elements[2]);
+        int testNumber = elements[0].codePointAt(0);  // Стартовое число (первое введенное) Unicode
+        //System.out.println(testNumber);
+        if (48 <= testNumber && testNumber <= 57) {             // Выбор из диапазона Unicode арабских цифр
+            int secondArabicNumber = elements[2].codePointAt(0);
+            if (48 <= secondArabicNumber && secondArabicNumber <= 57) {
+                a = Integer.parseInt(elements[0]);
+                b = Integer.parseInt(elements[2]);
+                c = elements[1].charAt(0);
+            } else {
+                throw new Exception("Something wrong..");
+            }
+        } else if (65 <= testNumber && testNumber <= 90) {          // Выбор из диапазона Unicode римских цифр
+            //System.out.println("It's a roman, baby..");
+            controlCheckbox = true;
+            for (Numeric n : romanNumbers) {
+                if (n.name().equals(elements[0])) {
+                    a = n.getArabic();
+                }
+            }
+            int secondRomanNumber = elements[2].codePointAt(0);
+            if (65 <= secondRomanNumber && secondRomanNumber <= 90) {
+                for (Numeric n : romanNumbers) {
+                    if (n.name().equals(elements[2])) {
+                        b = n.getArabic();
+                    }
+                }
+            } else {
+                throw new Exception("Something wrong..");
+            }
+            if (b > a) {
+                throw new Exception("Something wrong.."); // Контроль отрицательного результата для римских цифр
+            }
             c = elements[1].charAt(0);
-        } catch (NumberFormatException ex) {
-            ex.getMessage();
         }
 
         if (!(elements.length == 3)) {
-            System.out.println("Error, but i'll do it..");
+            // System.out.println("Error, but i'll do it..");
+            throw new Exception("Something wrong..");
         }
 
         if (a <= 10 & b <= 10) {
-            System.out.println(solution(a, b, c));
+            if (controlCheckbox) {            // Выводим результат римскими цифрами
+                input = transformationNumbers(solution(a, b, c), romanNumbers);
+                //System.out.println("Результат =  " + input);
+            } else {
+                input = String.valueOf(solution(a, b, c));             // Выводим результат арабскими цифрами
+                //System.out.println("Результат =  " + input);
+            }
         }
         return input;
     }
 
-    public static String solution(int a, int b, char c) {
+    public static int solution(int a, int b, char c) {
         int sum = 0;
         switch (c) {
             case '+':
@@ -52,7 +92,28 @@ public class Main {
                 sum = a / b;
                 break;
         }
-        return String.valueOf(sum);
+        return sum;
+    }
+
+    public static String transformationNumbers(int input, Numeric[] romanNumbers) {
+
+        String oneStr = "";
+        String tensStr = "";
+        String hundredStr = "";
+        int one = input % 10;
+        int tens = (input / 10) % 10 * 10;
+        int hundred = (input / 100) % 10 * 100;
+
+        for (Numeric n : romanNumbers) {
+            if (n.getArabic() == hundred) {
+                hundredStr = n.name();
+            } else if (n.getArabic() == tens) {
+                tensStr = n.name();
+            } else if (n.getArabic() == one) {
+                oneStr = n.name();
+            }
+        }
+        return hundredStr + tensStr + oneStr;
     }
 }
 
--- src/Numeric.java
+++ src/Numeric.java
@@ -9,11 +9,38 @@ public enum Numeric {
     VII(7),
     VIII(8),
     IX(9),
-    X(10);
+    X(10),
+    XI(11),
+    XII(12),
+    XX(20),
+    XXX(30),
+    XL(40),
+    L(50),
+    LX(60),
+    LXX(70),
+    LXXX(80),
+    XC(90),
+    C(100);
+//    CC(200),
+//    CCC(300),
+//    CD(400),
+//    D(500),
+//    DC(600),
+//    DCC(700),
+//    DCCC(800),
+//    CM(900),
+//    M(1000),
+//    MM(2000),
+//    MMM(3000),
+//    MMMCMXCIX(3999);
 
     private int arabic;
 
     Numeric(int arabic) {
         this.arabic = arabic;
     }
-}
+
+    public int getArabic() {
+        return arabic;
+    }
+    }
